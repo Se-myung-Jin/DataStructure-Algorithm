61aeb73a915cc54a61fe831d3ca3e1dff63deffa
Semyung Jin <tpaud94@naver.com>
2022-05-02 18:05:23 +0900

    우선순위 큐 추가

---
+++ DataStructureAndAlgorithm/Graph/PriorityQueue.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Graph
+{
+    class PriorityQueue
+    {
+        List<int> _heap = new List<int>();
+
+        public void Push(int data)
+        {
+            _heap.Add(data);
+
+            int now = _heap.Count - 1;
+
+            while (now > 0)
+            {
+                int next = (now - 1) / 2;
+
+                if (_heap[now] < _heap[next])
+                    break;
+
+                int temp = _heap[now];
+                _heap[now] = _heap[next];
+                _heap[next] = temp;
+
+                now = next;
+            }
+        }
+
+        public int Pop()
+        {
+            int ret = _heap[0];
+
+            int lastIndex = _heap.Count - 1;
+            _heap[0] = _heap[lastIndex];
+            _heap.RemoveAt(lastIndex--);
+
+            int now = 0;
+            while (true)
+            {
+                int left = now * 2 + 1;
+                int right = now * 2 + 2;
+
+                int next = now;
+                if (left <= lastIndex && _heap[next] < _heap[left])
+                    next = left;
+                if (right <= lastIndex && _heap[next] < _heap[right])
+                    next = right;
+
+                if (next == now)
+                    break;
+
+                int temp = _heap[now];
+                _heap[now] = _heap[next];
+                _heap[next] = temp;
+
+                now = next;
+            }
+
+            return ret;
+        }
+
+        public int Count()
+        {
+            return _heap.Count;
+        }
+    }
+}
--- DataStructureAndAlgorithm/Graph/Program.cs
+++ DataStructureAndAlgorithm/Graph/Program.cs
@@ -5,8 +5,17 @@
     {
         static void Main(string[] args)
         {
-            Graph graph = new Graph();
-            graph.Dijkstra(0);
+            PriorityQueue q = new PriorityQueue();
+            q.Push(20);
+            q.Push(10);
+            q.Push(30);
+            q.Push(90);
+            q.Push(40);
+
+            while (q.Count() > 0)
+            {
+                Console.WriteLine(q.Pop());
+            }
         }
     }
 }
